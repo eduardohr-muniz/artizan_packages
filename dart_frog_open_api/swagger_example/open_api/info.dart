98d51837beafb4bf4a6cdbfeab11d3ae59cf92a3
Eduardo Muniz <90477293+eduardohr-muniz@users.noreply.github.com>
2026-06-21 00:30:36 -0300

    refactor: update thermal_printer_flutter plugin structure and remove dart_frog_open_api example package

--- dart_frog_open_api/swagger_example/open_api/info.dart
+++
@@ -1,7 +0,0 @@
-import 'package:dart_frog_open_api/dart_frog_open_api.dart';
-
-const apiInfo = OpenApiInfo(
-  title: 'Swagger Example API',
-  description: 'Dart Frog + Zto DTO annotations demo.',
-  servers: ['http://localhost:8080'],
-);
